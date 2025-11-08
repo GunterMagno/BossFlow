2a9c13550f874bfa00e5039f4bdf4073f5bcaad8
Alejandro Borrego Cruz <aborcru560@g.educaand.es>
2025-11-08 11:40:15 +0100

    Instalado MongoDB y conectado correctamente

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
+const connectDB = require("./config/db");
 
+connectDB();
 app.use(express.json());
 
 const routes = require("./routes/index");
