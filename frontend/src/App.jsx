e56fe3524cc9daa3d205a1f039e5b2fab67d79fc
danielmi5 <midaniel@iesrafaelalberti.es>
2025-11-13 20:25:46 +0100

    Componente Navbar creado, con estilos usando las variables root y diseño responsive. Contiene estados para cambiar el diseño según inicio de sesión o no, y otro para mostrar o no el menú desplegable. Contiene un logo básico y links de navegación añadidos.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Navbar from './components/Navbar/Navbar'
 import Login from './components/Login/Login'
 import Register from './components/Register/Register'
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
