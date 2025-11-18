31836906338da312f8af1d1465f37ae1a446ab51
GunterMagno <aborcru560@g.educaand.es>
2025-11-18 14:19:48 +0100

    Añadido trim y required a los campos del esquema Diagram

--- backend/models/Diagram.js
+++ backend/models/Diagram.js
@@ -4,17 +4,20 @@ const DiagramSchema = new mongoose.Schema({
     title: {
         type: String,
         required: true,
+        trim: true,
         unique: true,
         minLength: 3,
   },
     description: {
         type: String,
         required: false,
+        trim: true,
         maxLength: 500,
     },
     userId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
+        required: true
     },
     nodes: {
         type: Array,
